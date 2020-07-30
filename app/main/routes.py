88f54edd4136cf5cb09da99b4e36ce9114ec8e40
bcparker21 <bcparker21@gmail.com>
2020-07-30 10:00:36 -0600

    add duty db fix

--- app/main/routes.py
+++ app/main/routes.py
@@ -228,8 +228,8 @@ def add_duty(title):
 	form=AddDutyForm(title=title)
 	if  form.validate_on_submit():
 		duty = Duty(body=form.duty, job_id=job.id)
-		db.session.add(duty)
-		db.session.commit()
+	db.session.add(duty)
+	db.session.commit()
 	return render_template('add_duty.html',form=form, title=title, duties=duties)
 
 @bp.route('/contact')
